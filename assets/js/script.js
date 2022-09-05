ef1704f5c00ef27c3767cc9a136f5d84c1f3c0ab
Alathrop2 <98856957+Alathrop2@users.noreply.github.com>
2022-09-05 13:16:39 -0600

    added global variables

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+var searchHistory = [];
+var searchCity = document.querySelector('.button');
+var apiKey = 'd21652416469d0c908812cedadf14a02';
+var historyContainer = document.getElementById('history');
