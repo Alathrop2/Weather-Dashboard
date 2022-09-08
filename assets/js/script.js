3f54a6589413e72b9dfa94bfc67ac5b2e856afb2
Alathrop2 <98856957+Alathrop2@users.noreply.github.com>
2022-09-07 18:29:16 -0600

    fixed local storage issues

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,8 @@ var searchCity = document.querySelector('.button');
 var apiKey = 'd21652416469d0c908812cedadf14a02';
 var historyContainer = document.getElementById('history');
 
+loadHistory();
+// display data functions
 var displayCurrentWeather = function (data) {
   var cityBox = $('.currentCityBox');
   var currentC = $('.currentC');
@@ -36,6 +38,7 @@ var displayCurrentWeather = function (data) {
   var image = $('<img>').attr('src', iconurl).addClass('image is-64x64 m-auto');
   currentC.append(image);
 };
+// 5 day forcast display info
 var fiveDayForecast = function (data) {
   var forecast = $('.forecast');
   if (forecast.children()) {
@@ -98,6 +101,14 @@ var cityInfo = function () {
       fiveDay(lat, lon);
       // local storage
       searchHistory.push(city);
+      saveHistory();
+      // loadHistory();
+      var historyButton = document.createElement('button');
+      historyButton.classList.add('is-size-5', 'button', 'is-link');
+      historyButton.setAttribute('type', 'button');
+      historyButton.setAttribute('id', 'historyItem');
+      historyButton.textContent = city;
+      historyContainer.append(historyButton);
     });
 };
 // 5 day forcast
@@ -114,4 +125,59 @@ var fiveDay = function (latitude, longitude) {
     .then((data) => fiveDayForecast(data));
 };
 
+// local storage
+
+var saveHistory = function () {
+  localStorage.setItem('Recent Searches', JSON.stringify(searchHistory));
+};
+function loadHistory() {
+  searchHistory = JSON.parse(localStorage.getItem('Recent Searches'));
+  if (!searchHistory) {
+    console.log(searchHistory);
+    searchHistory = [];
+  }
+  for (let i = 0; i < searchHistory.length; i++) {
+    var historyButton = document.createElement('button');
+    historyButton.classList.add('is-size-5', 'button', 'is-link');
+    historyButton.setAttribute('type', 'button');
+    historyButton.setAttribute('id', 'historyItem');
+    historyButton.textContent = searchHistory[i];
+    historyContainer.append(historyButton);
+  }
+}
+
 searchCity.addEventListener('click', cityInfo);
+let someVar = document.getElementsByClassName('is-link');
+for (let i = 0; i < someVar.length; i++) {
+  someVar[i].addEventListener('click', function (event) {
+    newCity(this.innerHTML);
+  });
+}
+var newCity = function (city) {
+  // var city = document.querySelector('#cityName').value;
+  var apiUrl =
+    'https://api.openweathermap.org/geo/1.0/direct?q=' +
+    city +
+    '&limit=5&appid=' +
+    apiKey;
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      var lat = data[0].lat;
+      var lon = data[0].lon;
+      getWeather(lat, lon);
+      fiveDay(lat, lon);
+    });
+};
+
+// GIVEN a weather dashboard with form inputs
+// WHEN I search for a city
+// * THEN I am presented with current and future conditions for that city and that city is added to the search history
+// WHEN I view current weather conditions for that city
+// * THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+// WHEN I view the UV index
+// * THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+// WHEN I view future weather conditions for that city
+// * THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+// WHEN I click on a city in the search history
+// * THEN I am again presented with current and future conditions for that city
